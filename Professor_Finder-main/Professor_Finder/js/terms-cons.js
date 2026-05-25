4b63c730901ff2ff1636745ce825486ddb563f25
dazt10 <dustin.kim0227@gmail.com>
2026-05-26 02:10:44 +0800

    Add files via upload

---
+++ Professor_Finder-main/Professor_Finder/js/terms-cons.js
@@ -0,0 +1,11 @@
+// BUTTON AND MODALS VARIABLE
+const modal = document.querySelector('.modal-bg');
+
+// OPEN TERMS FUNCTION
+function openTerms() {
+	modal.classList.toggle('show');
+}
+
+function closeTerms() {
+	modal.classList.toggle('show');
+}
